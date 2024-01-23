76625f9b158509d22949db86c289d826b74b1119
Lotta Tanner <lotta.tanner@helsinki.fi>
2024-01-23 14:08:32 +0200

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
-const App = () => (
+import './index.css'
+import matlu from './matlu.png'
+import FrontPage from './pages/frontpage'
+import React from 'react'
+
+const App = () => {
+
+  return(
   <div>
-    <p>Hello world</p>
+    <div id= 'blackscreen'>
+      <img src= {matlu} id="leftLogo" width="200" height="150"></img>
+    </div>
+
+    <FrontPage />
   </div>
-)
+)}
 
 export default App
---
+++ frontend/src/pages/frontpage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FrontPage = () => {
+    return (
+        <div>
+            <h1>Ilotalo</h1>
+            <p> ”sub hoc tecto cives academici excoluntur”?</p>
+            <p>Talon latinankielinen nimi, Domus Gaudium, tarkoittaa Ilo- tai Riemu-nimistä taloa. Se viittaa luontevasti opiskelun ja opiskelijaelämän riemuihin</p>
+            <p>ja jatkaa akateemista traditiota ja sopii paikan muuhun nimistöön, kuten Domus Academicaan. Talo sai myös oman tunnuslauseen,</p>
+            <p>"sub hoc tecto cives academici excoluntur”, joka suomennettuna tarkoittaa ”tämän rakennuksen suojissa tehdään akateemisia kansalaisia”.</p>
+        </div>
+    )
+}
+
+export default FrontPage
