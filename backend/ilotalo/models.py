fa9f1d96b5c28d4d321c98a1fd1069ba4b9a75bf
isabel <isabel.suorsa@helsinki.fi>
2024-01-24 12:34:55 +0200

    backend säätöö
    Co-authored-by: Valtteri Ahola <valtteri.ahola@helsinki.fi>
    Co-authored-by: Tatu Sorjonen <tatu.sorjonen@gmail.com>

--- backend/ilotalo/models.py
+++ backend/ilotalo/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
+    password = models.CharField(max_length=20, default="")
     role = models.IntegerField()
