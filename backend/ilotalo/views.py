c6f671ea4853f3b8b82f6c3c2e85a6eb1700af2b
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2024-01-29 13:01:18 +0200

    axiosin testailua
    Co-authored by: Lotta Tanner <lotta.tanner@helsinki.fi>
    Co-authored by: Ada <ada.rautiainen@gmail.com>
    Co-authored by: Tatu Sorjonen <tatu.sorjonen@gmail.com>

--- backend/ilotalo/views.py
+++ backend/ilotalo/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import pymongo
 from rest_framework import viewsets
 from .serializers import UserSerializer
 from .models import User
-from django.http import HttpResponse
+from .utils import db_connection
 
 # Create your views here.
 
 class IndexView(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+connection = db_connection(
+    "<mongodb url here>",
+    "softwareproject",
+    "users",
+)
+
+db_collection = connection[2]
+
+user = {"username": "Valtteri", "password": "12345", "role": 5}
+
+db_collection.insert_one(user)
--- frontend/src/pages/loginpage.jsx
+++ frontend/src/pages/loginpage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios'
 import '../index.css'
 
 const LoginPage = () => {
@@ -7,6 +8,31 @@ const LoginPage = () => {
   const [loggedIn, setLoggedIn] = useState(false);
 
 
+  //esimerkkifunktio datan hakemiselle rajapinnasta
+  const getData = () => {
+    axios
+    .get('http://localhost:8000/users/')
+    .then(response => {
+      const data = response.data
+      //setUsername(data[1].username)
+      console.log(response.data)
+    })
+  }
+
+  //esimerkkifunktio datan lähettämiselle rajapintaan
+  const sendData = () => {
+    const userObject = {
+      username: username,
+      password: password,
+      role: 5
+    }
+  
+    axios
+      .post('http://localhost:8000/users/', userObject)
+      .then(response => {
+        console.log(response)
+      })
+  }
 
   const handleNewUser = () => {
     console.log('New user registration...');
