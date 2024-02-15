fd4d55c3e1730b50fee111a967485c49dc247b1b
Tatu Sorjonen <tatu.sorjonen@gmail.com>
2024-02-15 14:00:43 +0200

    käyttäjänhallinta tehty jollain tasolla
    Co-authored-by: Valtteri Ahola <valtteri.ahola@helsinki.fi>

--- backend/ilotalo/urls.py
+++ backend/ilotalo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, RetrieveUserView
 
 urlpatterns = [
-    path('register', RegisterView.as_view())
+    path('register', RegisterView.as_view()),
+    path('userlist', RetrieveUserView.as_view()),
 ]
--- backend/ilotalo/views.py
+++ backend/ilotalo/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from .serializers import UserSerializer, OrganizationSerializer
+from .serializers import UserSerializer, OrganizationSerializer, UserNoPasswordSerializer
 from .models import User, Organization
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -19,14 +19,22 @@ class OrganizationView(viewsets.ModelViewSet):
 class RegisterView(APIView):
     def post(self, request):
         data = request.data
+        '''
         username = data['username']
         password = data['password']
         email = data['email']
         telegram = data['telegram']
         role = data['role']
+        '''
 
-        user = User.objects.create_user(username, password, email, telegram, role)
-        user = UserSerializer(user)
+        serializer = UserSerializer(data=data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        #user = User.objects.create_user(username, password, email, telegram, role)
+        #user = UserSerializer(user)
+        user = serializer.create(serializer.validated_data)
+        user = UserNoPasswordSerializer(user)
 
 
         return Response(user.data, status=status.HTTP_201_CREATED)
@@ -34,5 +42,8 @@ class RegisterView(APIView):
 class RetrieveUserView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def post(self, request):
-        pass
+    def get(self, request):
+        user = request.user
+        user = UserSerializer(user)
+
+        return Response(user.data, status=status.HTTP_200_OK)
