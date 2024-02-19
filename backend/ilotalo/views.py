610c2ee0e9e7d573c313a06384535ae468fd2fb8
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2024-02-19 15:59:22 +0200

    kommentteja backendin koodiin

--- backend/ilotalo/views.py
+++ backend/ilotalo/views.py
@@ -9,30 +9,31 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions, status
 
-# Create your views here.
-
-# Comment
-
 
 class UserView(viewsets.ModelViewSet):
+    """Displays a list of all User objects at <baseurl>/users/"""
+
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 
 class OrganizationView(viewsets.ModelViewSet):
+    """Displays a list of all Organization objects at <baseurl>/organizations/"""
+
     serializer_class = OrganizationSerializer
     queryset = Organization.objects.all()
 
 
 class RegisterView(APIView):
+    """View for creating a new user at <baseurl>/api/users/register/"""
+
     def post(self, request):
         data = request.data
         serializer = UserSerializer(data=data)
 
+        # Check if the request contains valid data
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        # user = User.objects.create_user(username, password, email, telegram, role)
-        # user = UserSerializer(user)
         user = serializer.create(serializer.validated_data)
         user = UserNoPasswordSerializer(user)
 
@@ -40,6 +41,9 @@ class RegisterView(APIView):
 
 
 class RetrieveUserView(APIView):
+    """View for fetching a User object with a JSON web token at <baseurl>/api/users/userlist/"""
+
+    #IsAuthenticated permission class will deny entry for unauthenticated users
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
