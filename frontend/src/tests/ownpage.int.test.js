d44091f1b6434c7370c517894636707fde9277df
Lotta Tanner <lotta.tanner@helsinki.fi>
2024-02-23 14:17:42 +0200

    pieni css muokkaus ja testejä ownpagelle

---
+++ frontend/src/tests/ownpage.int.test.js
@@ -0,0 +1,38 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import axios from 'axios';
+import OwnPage from '../../src/pages/ownpage';
+
+jest.mock('axios');
+
+describe('OwnPage', () => {
+    beforeEach(() => {
+      axios.get.mockResolvedValue({ data: [] }); 
+    });
+  
+    afterEach(() => {
+        jest.restoreAllMocks();
+      })
+    
+    it('renders login message if not logged in', () => {
+        render(<OwnPage isLoggedIn={false} />);
+        expect(screen.getByText('Kirjaudu ensin sisään')).toBeTruthy();
+      });
+    
+    it('renders user page if logged in', async () => {
+        render(<OwnPage isLoggedIn={true} />);
+        
+        await waitFor(() => {
+            expect(screen.getByText('Käyttäjänimi:')).toBeTruthy()
+            expect(screen.getByText('Sähköposti:')).toBeTruthy()
+            expect(screen.getByText('Telegram:')).toBeTruthy()
+            expect(screen.getByText('Rooli: 5')).toBeTruthy()
+            expect(screen.getByText('Virka:')).toBeTruthy()
+            expect(screen.getByText('Myöntöpäivä:')).toBeTruthy()
+            expect(screen.getByText('Järjestöt')).toBeTruthy();
+        });
+      })
+
+})
+
+
+    
