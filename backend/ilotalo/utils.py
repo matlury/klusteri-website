5a8bdab824bbe3b19698c73d2ae7a6aac22df597
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2024-02-26 15:02:29 +0200

    turhasta koodista eroon

--- backend/ilotalo/utils.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-
-def db_connection(url: str, db_name: str, collection_name: str):
-    client = MongoClient(url)
-    db_handle = client[f"{db_name}"]
-    collection = db_handle[f"{collection_name}"]
-    return db_handle, client, collection
