ff05a7de74af482f8820ea5b2d67db20573ea2b0
Lotta Tanner <lotta.tanner@helsinki.fi>
2024-02-26 15:41:03 +0200

    sivujen muokkausta ja login/logout toimii reaaliajassa

---
+++ frontend/src/pages/privacypolicy.jsx
@@ -0,0 +1,49 @@
+import '../index.css'
+import React from 'react'
+
+const PrivacyPolicy = () => {
+    return (
+        <div className='textbox'>
+            <h1>Tietosuojaseloste</h1>
+            <p>Tämä on EU:n yleisen tietosuoja-asetuksen mukainen rekisteri- ja tietosuojaseloste. Laatimispäivämäärä on 17.5.2018. Viimeisin muutos 17.1.2022</p>
+            <h2>1. Rekisterinpitäjä</h2>
+            <p>Helsingin yliopiston matemaattis-luonnontieteellisten opiskelijajärjestöjen yhteistyöjärjestö Matlu ry</p>
+            <p>Leppäsuonkatu 11, 00100 Helsinki</p>
+            <h2>2. Yhteyshenkilö rekisteriä koskevissa asioissa</h2>
+            <p>Helsingin yliopiston matemaattis-luonnontieteellisten opiskelijajärjestöjen yhteistyöjärjestö Matly ry:n Leppätalokomitean puheenjohtaja (Olli Keski-Hynnilä vuonna 2022) olli.keski-hynnila@helsinki.fi</p>
+            <h2>Rekisterin nimi</h2>
+            <p>Christina Reginan kulkuavainrekisteri</p>
+            <h2>4. Oikeusperuste ja henkilötietojen käsittelyn tarkoitus</h2>
+            <p>EU:n yleisen tietosuoja-asetuksen mukainen oikeusperuste henkilötietojen käsittelylle ovat rekisterinpitäjän oikeutettu etu ja laillinen velvoite.</p>
+            <p>Henkilötietojen käsittelyn tarkoitus on ylläpitää kirjaa avaimellisista sekä huolehtia heidän koulutuksestaan tilan käyttöä varten. </p>
+            <h2>5. Rekisterin tietosisältö</h2>
+            <p>Etu- ja sukunimi</p>
+            <br />Sähköpostiosoite<br />
+            <br />Järjestö, jonka puolesta on saanut Christina Reginaan kulkuavaimen<br />
+            <br />Viimeisin Christina Reginan käyttökulutusvuosi<br />
+            <br />*Rike<br />
+            <br />*Sanktio<br />
+            <br />Käyttökiellon viimeinen päivä<br />
+            <p>(*Tähdellä merkittyjä kenttiä käytetään vain tarvittaessa)</p>
+            <h2>6. Säännönmukaiset tietolähteet</h2>
+            <p>Jäsenjärjestöjen antamat listaukset myönnetyistä Christina Reginan kulkuavaimista.</p>
+            <h2>7. Tietojen säännönmukaiset luovutukset</h2>
+            <p>Tietoja luovutetaan Matly ry:n jäsenjärjestöjen puheenjohtajien pyynnöstä sähköpostitse.</p>
+            <h2>8. Tietojen siirto EU:n tai ETA:n ulkopuolelle</h2>
+            <p>Tietoja käsitellään Googlen pilvipalveluissa, jolloin käsiteltävät tiedot voivat sijaita EU:n tai ETA:n ulkopuolella. Google on sitoutunut noudattamaan pilvipalvelujensa osalta EU:n yleistä tietosuoja-asetusta ja Privacy Shield -viitekehystä.</p>
+            <p>Edellä mainitun lisäksi tietoja ei siirretä Euroopan unionin tai Euroopan talousalueen ulkopuolelle</p>
+            <h2>9. Rekisterin suojauksen periaatteet</h2>
+            <p>Tietoja säilytetään Googlen pilvipalveluissa. Pääsy Googlen pilvipalveluihin on yhdistyksen hallituslaisilla, sekä rajatusti yhdistyksen aktiiveilla. Googlen pilvipalveluiden käyttö tapahtuu henkilökohtaisilla käyttäjätunnuksilla ja salasanoilla.</p>
+            <h2>10. Tarkastusoikeus</h2>
+            <p>Jokaisella rekisteriin kuuluvalla henkilöllä on oikeus tarkistaa rekisteriin hänestä tallennetut tiedot. Tietojen tarkistuspyyntö tulee lähettää kirjallisesti rekisterinpitäjälle. Rekisterinpitäjällä on tarvittaessa oikeus pyytää pyynnön esittäjää todistamaan henkilöllisyytensä. Rekisterinpitäjä vastaa pyynnön esittäjälle EU:n tietosuoja-asetuksessa säädetyssä ajassa (pääsääntöisesti kuukauden kuluessa). Tarkastaminen on maksutonta kerran vuodessa</p>
+            <h2>11. Oikeus vaatia tiedon korjaamista</h2>
+            <p>Jokaisella rekisteriin kuuluvalla henkilöllä on oikeus vaatia rekisteriin hänestä talletettujen tietojen korjausta. Tietojen korjauspyyntö tulee lähettää kirjallisesti rekisterinpitäjälle. Rekisterinpitäjällä on tarvittaessa oikeus pyytää pyynnön esittäjää todistamaan henkilöllisyytensä. Rekisterinpitäjä toteuttaa esittäjän pyynnön EU:n tietosuoja-asetuksessa säädetyssä ajassa (pääsääntöisesti kuukauden kuluessa)</p>
+            <h2>12. Muut henkilötietojen käsittelyyn liittyvät tiedot</h2>
+            <p>Tiedot hävitetään vuosittain heti alkuvuodesta siten, että ne, joilla tulee kaksi (2) vuotta viimeisimmästä Christina Reginan käyttökoulutusvuodesta, poistetaan rekisteristä. Tietoja siis säilytetään vähintään kahden vuoden ajan.</p>
+            <h2>Poikkeus:</h2>
+            <p>Mikäli rekisteröity on toiminut vastoin sääntöjä ja hänelle on Leppätalokomitean valtuuksilla annettu käyttökielto Christina Reginaan, kirjataan rekisteröidylle rike, sanktio ja käyttökiellon viimeinen päivämäärä. Tietoja säilytetään käyttökiellon viimeiseen päivämäärään saakka tai 10 vuoden ajan, kumpi näistä on lyhyempi.</p>            
+        </div>
+    )
+}
+
+export default PrivacyPolicy
