11fbec7bc715b55422efd91ca9c840c5e6501624
Anna Essina <anna.essina@helsinki.fi>
2024-03-04 14:43:32 +0200

    kalenterin rakentelua

--- frontend/src/pages/reservations.jsx
+++ frontend/src/pages/reservations.jsx
@@ -1,12 +1,48 @@
-import '../index.css'
-import React from 'react'
+import React, { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import '../index.css';
+
+const localizer = momentLocalizer(moment);
+
+const MyCalendar = () => {
+  const [events, setEvents] = useState([]);
+
+  const handleSelectSlot = ({ start, end }) => {
+    const title = window.prompt('Syötä tapahtuman nimi:');
+    const organizer = window.prompt('Syötä järjestäjä:');
+    const isOpen = window.confirm('Onko varaus avoin vai suljettu?');
+    if (title && organizer !== null) {
+      const newEvent = {
+        start,
+        end,
+        title,
+        organizer,
+        isOpen,
+      };
+      setEvents([...events, newEvent]);
+    }
+  };
+
+  return (
+    <div className="textbox">
+      <h1>Varauskalenteri</h1>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        selectable
+        onSelectSlot={handleSelectSlot}
+      />
+    </div>
+  );
+};
 
 const Reservations = () => {
-    return (
-        <div className= 'textbox'>
-            <h1>Sivua rakennetaan vielä! :)</h1>
-        </div>
-    )
-}
+  return <MyCalendar />;
+};
 
-export default Reservations
+export default Reservations;
