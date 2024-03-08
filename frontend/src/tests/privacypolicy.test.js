300530960b774d6798ae690ddae9c590da23f8c7
Lotta Tanner <lotta.tanner@helsinki.fi>
2024-03-08 17:38:09 +0200

    fronttitestejä

---
+++ frontend/src/tests/privacypolicy.test.js
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import PrivacyPolicy from '../../src/pages/privacypolicy'
+
+test('renders Privacy Policy page component', () => {
+  const { getByText } = render(<PrivacyPolicy />)
+
+  expect(getByText('Tietosuojaseloste')).toBeInTheDocument()
+  
+  expect(getByText('1. Rekisterinpitäjä')).toBeInTheDocument()
+})
