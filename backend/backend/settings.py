5b27ae56d18d93cb05780e72366b3b41b3499a49
isabel <isabel.suorsa@helsinki.fi>
2024-03-08 15:07:01 +0200

    frontin url settingsiin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -167,4 +167,4 @@ AUTH_USER_MODEL = "ilotalo.User"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-CORS_ORIGIN_WHITELIST = ["http://localhost:5173"]
+CORS_ORIGIN_WHITELIST = ["http://localhost:5173", "https://klusteri-website-front-matlury-test.apps.ocp-test-0.k8s.it.helsinki.fi/"]
