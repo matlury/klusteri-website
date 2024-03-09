10d96e8e677ee6fab5802b0a5b788eaa1d571b05
Laura Peltonen <laura.peltonen53@gmail.com>
2024-03-09 14:49:01 +0200

    Make tests for night responsibility

--- backend/tests/test_models.py
+++ backend/tests/test_models.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from ilotalo.models import User
-
+from ilotalo.models import NightResponsibility, User
 
 # Create your tests here.
 class UserTest(TestCase):
@@ -45,3 +44,25 @@ class UserTest(TestCase):
 
         with self.assertRaises(ValidationError):
             new_user.full_clean()
+
+class NightResponsibilityTestCase(TestCase):
+        # Creating a new NightResponsibility object via test data
+    def setUp(self):
+        self.night_responsibility = NightResponsibility.objects.create(
+            username="test_user",
+            email="test@example.com",
+            responsible_for="Testing duties",
+            login_time="1970-01-01T12:00:00",
+            logout_time="1970-01-02T14:00:00",
+            present=True,
+            late=False
+        )
+    # Testing the creation of a NightResponsibility object and the correctness of its attributes
+    def test_night_responsibility_creation(self):
+        self.assertEqual(self.night_responsibility.username, "test_user")
+        self.assertEqual(self.night_responsibility.email, "test@example.com")
+        self.assertEqual(self.night_responsibility.responsible_for, "Testing duties")
+        self.assertEqual(str(self.night_responsibility.login_time), "1970-01-01T12:00:00")
+        self.assertEqual(str(self.night_responsibility.logout_time), "1970-01-02T14:00:00")
+        self.assertTrue(self.night_responsibility.present)
+        self.assertFalse(self.night_responsibility.late)
