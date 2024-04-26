8cd915a539ff134dd45f42b882ca13899f4b440a
Ada <114645764+adarautiainen@users.noreply.github.com>
2024-04-26 09:24:20 +0300

    lisätty front openshift osoite

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,8 @@ ALLOWED_HOSTS = [
     "klusteri-website-db-test-matlury-test.apps.ocp-test-0.k8s.it.helsinki.fi",
     "klusteri-website-back-matlury-test.apps.ocp-prod-0.k8s.it.helsinki.fi",
     "klusteri-website-front-matlury-test.apps.ocp-prod-0.k8s.it.helsinki.fi",
-    "klusteri-website-matlury-test.apps.ocp-prod-0.k8s.it.helsinki.fi"
+    "klusteri-website-matlury-test.apps.ocp-prod-0.k8s.it.helsinki.fi",
+    "klusteri-website-front-matlury-test.apps.ocp-test-0.k8s.it.helsinki.fi"
 ]
 
 
