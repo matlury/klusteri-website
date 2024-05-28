afb9e58bfe7bb551826963b97daab471e709931e
oskarruo <oskar.ruokolainen@gmail.com>
2024-05-28 15:40:10 +0300

    testitapaus ykv filtterille

--- frontend/src/tests/ownkeys.test.js
+++ frontend/src/tests/ownkeys.test.js
@@ -103,4 +103,80 @@ describe("OwnKeys Component", () => {
       );
     });
   });
+
+  it("filtering works", async () => {
+    const user = {
+      username: "example_username",
+      email: "example_email@example.com",
+      telegram: "example_telegram",
+      role: 1,
+      keys: {"tko-äly": true},
+      organization: {"tko-äly": true},
+      rights_for_reservation: true,
+    };
+
+    window.confirm = jest.fn(() => true);
+    localStorage.setItem("ACCESS_TOKEN", "example_token");
+    localStorage.setItem("loggeduser", JSON.stringify(user));
+
+    const { getByText, getByLabelText, getByTestId, queryByText } = render(
+      <OwnKeys isLoggedIn={true} loggedUser={user} />,
+    );
+
+    let response = { data: [
+      {
+        "id": 46,
+        "username": "example_username",
+        "email": "example_username",
+        "responsible_for": "fuksit",
+        "login_time": "2024-05-21T20:13:00Z",
+        "logout_time": "2024-05-27T12:17:30.940903Z",
+        "present": true,
+        "late": false,
+        "created_by": "example_username",
+        "organisations": "tko-äly"
+    },
+    {
+      "id": 47,
+      "username": "example_username",
+      "email": "example_username",
+      "responsible_for": "barney",
+      "login_time": "2024-05-21T20:14:00Z",
+      "logout_time": "2024-05-27T12:17:30.940903Z",
+      "present": true,
+      "late": false,
+      "created_by": "example_username",
+      "organisations": "tko-äly"
+  }
+    ]}
+    const responsedata = { data: {
+      "id": 1,
+      "username": "example_username",
+      "password": "pbkdf2_sha256$720000$dvnLXQXvNU0szCaM2tdeVy$kVehEN6LlYfyjbHp3TwwNwRAMfzirXnXuFCAbhybGWE=",
+      "email": "example_email@example.com",
+      "telegram": "example_telegram",
+      "role": 1,
+      "organization": {
+          "tko-äly": true
+      },
+      "keys": {
+          "tko-äly": true
+      },
+      "rights_for_reservation": true
+    }}
+
+    await waitFor(() => {
+    mockAxios.mockResponseFor({url: 'listobjects/nightresponsibilities/'}, response)
+    mockAxios.mockResponseFor({url: 'undefined/api/users/userinfo'}, responsedata)
+
+      expect(mockAxios.get).toHaveBeenCalledWith('undefined/api/users/userinfo', {"headers": {"Authorization": "Bearer example_token"}})
+      expect(mockAxios.get).toHaveBeenCalledWith('listobjects/nightresponsibilities/')
+
+      const filter = getByTestId("ykvfiltersearch")
+      fireEvent.change(filter, { target: { value: "fuksit" } })
+
+      expect(getByText("Vastuussa henkilöistä: fuksit")).toBeInTheDocument();
+      expect(queryByText('Vastuussa henkilöistä: barney')).toBeNull()
+    })
+  })
 });
