84df6c51f22a1c5f7745c61e53f9d33f9b4c009e
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-05-29 09:31:32 +0300

    Modify from class component to functional component

--- frontend/src/components/YkvLogoutFunction.jsx
+++ frontend/src/components/YkvLogoutFunction.jsx
@@ -1,25 +1,19 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axiosClient from "../axios.js";
 import { DataGrid } from '@mui/x-data-grid';
 import { Button } from "@mui/material";
 
+const YkvLogoutFunction = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-class YkvLogoutFunction extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-      loading: true,
-    };
+  const columns = [
+    { field: 'Vastuuhenkilö', headerName: 'Vastuuhenkilö', width: 200 },
+    { field: 'Vastuussa', headerName: 'Vastuussa', width: 300 },
+    { field: 'YKV_sisäänkirjaus', headerName: 'YKV sisäänkirjaus', width: 200 },
+  ];
 
-    this.columns = [
-      { field: 'Vastuuhenkilö', headerName: 'Vastuuhenkilö', width: 200 },
-      { field: 'Vastuussa', headerName: 'Vastuussa', width: 300 },
-      { field: 'YKV_sisäänkirjaus', headerName: 'YKV sisäänkirjaus', width: 200 },
-    ];
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     axiosClient
       .get("/listobjects/nightresponsibilities/")
       .then((res) => {
@@ -29,41 +23,34 @@ class YkvLogoutFunction extends Component {
           Vastuussa: u.responsible_for,
           YKV_sisäänkirjaus: u.login_time, // Assuming login_time is available
         }));
-        this.setState({
-          users: userData,
-          loading: false,
-        });
+        setUsers(userData);
+        setLoading(false);
       })
       .catch((error) => console.error(error));
-  }
+  }, []);
 
-  render() {
-    return this.state.loading ? (
-      <div>Lataa...</div>
-    ) : (
-      
-      <div style={{ height: 400, width: '100%' }}>
-        <h2>Aktiiviset</h2>
-        <Button
+  return loading ? (
+    <div>Lataa...</div>
+  ) : (
+    <div style={{ height: 400, width: '100%' }}>
+      <h2>Aktiiviset</h2>
+      <Button
         variant="contained"
         className="login-button"
         color="primary"
         type="submit"
-        >
-          + Ota vastuu
-        </Button>
-        
-
-        <DataGrid
-          rows={this.state.users}
-          columns={this.columns}
-          pageSize={5}
-          rowsPerPageOptions={[5, 10, 20]}
-          checkboxSelection={true}
-        />
-      </div>
-    );
-  }
-}
+      >
+        + Ota vastuu
+      </Button>
+      <DataGrid
+        rows={users}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5, 10, 20]}
+        checkboxSelection
+      />
+    </div>
+  );
+};
 
 export default YkvLogoutFunction;
