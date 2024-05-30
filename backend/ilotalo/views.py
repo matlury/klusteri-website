e1aed6a0ff9a049d8669aad8bff1cd307ccfe356
oskera <oskari.e.keranen@gmail.com>
2024-05-30 12:22:36 +0300

    lisää avainten lukumäärä järjestösivulle

--- backend/ilotalo/views.py
+++ backend/ilotalo/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions, status
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q
+from django.db.models import Q, Count
 from .serializers import (
     UserSerializer,
     OrganizationSerializer,
