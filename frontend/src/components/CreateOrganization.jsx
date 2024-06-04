56b2bc1e794e2a0c0a910e7e6fffe9d4a50574ff
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-06-04 14:02:51 +0300

    Correct className

--- frontend/src/components/CreateOrganization.jsx
+++ frontend/src/components/CreateOrganization.jsx
@@ -28,7 +28,7 @@ const CreateOrganization = ({
         <TextField
           id="email"
           label="Sähköposti"
-          class="organization-email"
+          className="organization-email"
           value={organization_email}
           onChange={(e) => setOrganizationEmail(e.target.value)}
         />
