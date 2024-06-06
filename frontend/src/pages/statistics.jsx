57b871aa05a93800f4655303c696056fc9c4c892
oskarruo <oskar.ruokolainen@gmail.com>
2024-06-06 13:11:01 +0300

    korjattu viikonpäivät

--- frontend/src/pages/statistics.jsx
+++ frontend/src/pages/statistics.jsx
@@ -20,34 +20,46 @@ const Statistics = () => {
   const [allUserStatsData, setAllUserStatsData] = useState([]);
   const [CSVdata, setCSVdata] = useState(null);
   const [shouldDownload, setShouldDownload] = useState(false);
-  const [minFilter, setMinFilter] = useState("")
-  const [maxFilter, setMaxFilter] = useState("")
-  const [fetchedData, setFetchedData] = useState(null)
-  const [logTimesData, setLogTimesData] = useState(null)
-  const [winHeight, setWinHeight] = useState(window.innerHeight)
-  const [winWidth, setWinWidth] = useState(window.innerWidth)
-  const [columnWidth, setColumnWidth] = useState(6)
-  const [widthDivider, setWidthDivider] = useState(2.5)
-  const [logsPerWeekDayData, setLogsPerWeekDayData] = useState(null)
+  const [minFilter, setMinFilter] = useState("");
+  const [maxFilter, setMaxFilter] = useState("");
+  const [fetchedData, setFetchedData] = useState(null);
+  const [logTimesData, setLogTimesData] = useState(null);
+  const [winHeight, setWinHeight] = useState(window.innerHeight);
+  const [winWidth, setWinWidth] = useState(window.innerWidth);
+  const [columnWidth, setColumnWidth] = useState(6);
+  const [widthDivider, setWidthDivider] = useState(2.5);
+  const [logsPerWeekDayData, setLogsPerWeekDayData] = useState(null);
 
   useEffect(() => {
     getPermission();
     if (fetchedData) {
-      processOrgStats(fetchedData.orgResponse.data, fetchedData.responsibilitiesResponse.data);
-      processAllUserStats(fetchedData.userResponse.data, fetchedData.responsibilitiesResponse.data);
+      processOrgStats(
+        fetchedData.orgResponse.data,
+        fetchedData.responsibilitiesResponse.data,
+      );
+      processAllUserStats(
+        fetchedData.userResponse.data,
+        fetchedData.responsibilitiesResponse.data,
+      );
     } else if (localStorage.getItem("ACCESS_TOKEN")) {
       fetchData().then(setFetchedData);
     }
     if (window.innerWidth <= window.innerHeight) {
-      setColumnWidth(12)
-      setWidthDivider(1.2)
+      setColumnWidth(12);
+      setWidthDivider(1.2);
     }
   }, []);
 
   useEffect(() => {
     if (fetchedData) {
-      processOrgStats(fetchedData.orgResponse.data, fetchedData.responsibilitiesResponse.data);
-      processAllUserStats(fetchedData.userResponse.data, fetchedData.responsibilitiesResponse.data);
+      processOrgStats(
+        fetchedData.orgResponse.data,
+        fetchedData.responsibilitiesResponse.data,
+      );
+      processAllUserStats(
+        fetchedData.userResponse.data,
+        fetchedData.responsibilitiesResponse.data,
+      );
     }
   }, [minFilter, maxFilter, fetchedData]);
 
@@ -56,18 +68,18 @@ const Statistics = () => {
       setWinWidth(window.innerWidth);
       setWinHeight(window.innerHeight);
       if (window.innerWidth <= window.innerHeight) {
-        setColumnWidth(12)
-        setWidthDivider(1.2)
+        setColumnWidth(12);
+        setWidthDivider(1.2);
       } else {
-        setColumnWidth(6)
-        setWidthDivider(2.5)
+        setColumnWidth(6);
+        setWidthDivider(2.5);
       }
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -102,15 +114,17 @@ const Statistics = () => {
   };
 
   function filtering(login_time, logout_time) {
-    return ((Date.parse(login_time) > Number(Date.parse(minFilter)) && 
-    Date.parse(login_time) < Number(Date.parse(maxFilter))) ||
-    (Date.parse(logout_time) < Number(Date.parse(maxFilter)) &&
-    Date.parse(logout_time) > Number(Date.parse(minFilter))) ||
-    (Date.parse(login_time) > Number(Date.parse(minFilter)) &&
-    maxFilter === "") ||
-    (Date.parse(logout_time) < Number(Date.parse(maxFilter)) &&
-    minFilter === "") || (minFilter === "" && maxFilter ==="")
-    )  
+    return (
+      (Date.parse(login_time) > Number(Date.parse(minFilter)) &&
+        Date.parse(login_time) < Number(Date.parse(maxFilter))) ||
+      (Date.parse(logout_time) < Number(Date.parse(maxFilter)) &&
+        Date.parse(logout_time) > Number(Date.parse(minFilter))) ||
+      (Date.parse(login_time) > Number(Date.parse(minFilter)) &&
+        maxFilter === "") ||
+      (Date.parse(logout_time) < Number(Date.parse(maxFilter)) &&
+        minFilter === "") ||
+      (minFilter === "" && maxFilter === "")
+    );
   }
 
   const processOrgStats = (orgData, responsibilities) => {
@@ -121,11 +135,11 @@ const Statistics = () => {
     responsibilities.forEach((resp) => {
       resp.organizations.forEach((org) => {
         if (filtering(resp.login_time, resp.logout_time)) {
-        orgdata[org.name] = {
-          ...orgdata[org.name],
-          value: orgdata[org.name].value + 1,
-        };
-      }
+          orgdata[org.name] = {
+            ...orgdata[org.name],
+            value: orgdata[org.name].value + 1,
+          };
+        }
       });
     });
     const realdata = Object.values(orgdata);
@@ -134,30 +148,32 @@ const Statistics = () => {
 
   const processAllUserStats = (users, responsibilities) => {
     const userdata = {};
-    
-    const logintimesdata = new Array(24).fill(0)
-    const logouttimesdata = new Array(24).fill(0)
-    
-    const lpddata = new Array(7).fill(0)
-    
+
+    const logintimesdata = new Array(24).fill(0);
+    const logouttimesdata = new Array(24).fill(0);
+
+    const lpddata = new Array(7).fill(0);
+
+    const numberdayweek = [6, 0, 1, 2, 3, 4, 5];
+
     users.forEach((usr) => {
       userdata[usr.username] = { data: [0], label: usr.username };
     });
     responsibilities.forEach((resp) => {
       if (userdata[resp.user.username]) {
         if (filtering(resp.login_time, resp.logout_time)) {
-        userdata[resp.user.username] = {
-          ...userdata[resp.user.username],
-          data: [userdata[resp.user.username].data[0] + 1],
-        };
-        const loginhours = new Date(resp.login_time).getHours()
-        const logouthours = new Date(resp.logout_time).getHours()
-        logintimesdata[loginhours] += 1
-        logouttimesdata[logouthours] += 1
-        
-        const loginweekday = new Date(resp.login_time).getDay()
-        lpddata[loginweekday] += 1
-      }
+          userdata[resp.user.username] = {
+            ...userdata[resp.user.username],
+            data: [userdata[resp.user.username].data[0] + 1],
+          };
+          const loginhours = new Date(resp.login_time).getHours();
+          const logouthours = new Date(resp.logout_time).getHours();
+          logintimesdata[loginhours] += 1;
+          logouttimesdata[logouthours] += 1;
+
+          const day = new Date(resp.login_time).getDay();
+          lpddata[numberdayweek[day]] += 1;
+        }
       }
     });
     Object.values(userdata).forEach((usr) => {
@@ -165,14 +181,21 @@ const Statistics = () => {
         delete userdata[usr.label];
       }
     });
-    const logs = [{data: logintimesdata, label:"Sisäänkirjautuminen", color:'lightGreen'}, {data: logouttimesdata, label:"Uloskirjautuminen", color:'red'}]
-    setLogTimesData(logs)
-    console.log(logs)
-    
-    const logsperday = [{ data: lpddata}]
-    setLogsPerWeekDayData(logsperday)
-    console.log(logsperday)
-    
+    const logs = [
+      {
+        data: logintimesdata,
+        label: "Sisäänkirjautuminen",
+        color: "lightGreen",
+      },
+      { data: logouttimesdata, label: "Uloskirjautuminen", color: "red" },
+    ];
+    setLogTimesData(logs);
+    console.log(logs);
+
+    const logsperday = [{ data: lpddata }];
+    setLogsPerWeekDayData(logsperday);
+    console.log(logsperday);
+
     const realdata = Object.values(userdata);
     realdata.sort(
       (a, b) =>
@@ -180,7 +203,7 @@ const Statistics = () => {
         parseFloat(a.data.reduce((partialSum, a) => partialSum + a, 0)),
     );
     setAllUserStatsData(realdata);
-    console.log(realdata)
+    console.log(realdata);
   };
 
   const handleCSV = async () => {
@@ -252,92 +275,82 @@ const Statistics = () => {
     <div>
       <Grid container spacing={2}>
         <Grid item xs={columnWidth}>
-        <div>
-        <p>Hae aikavälillä</p>
-        <input type="hidden" id="timezone" name="timezone" value="03:00" />
-        <input
-          value={minFilter}
-          onChange={handleMinFilterChange}
-          type="datetime-local"
-        />
-        <input
-          value={maxFilter}
-          onChange={handleMaxFilterChange}
-          type="datetime-local"
-        />
-        </div>
+          <div>
+            <p>Hae aikavälillä</p>
+            <input type="hidden" id="timezone" name="timezone" value="03:00" />
+            <input
+              value={minFilter}
+              onChange={handleMinFilterChange}
+              type="datetime-local"
+            />
+            <input
+              value={maxFilter}
+              onChange={handleMaxFilterChange}
+              type="datetime-local"
+            />
+          </div>
         </Grid>
         <Grid item xs={columnWidth}>
-        <div
-        style={{float: "right"}}>
-        <Button type="button" variant="contained" onClick={handleCSV}>
-          Lataa CSV-tiedosto tapahtumista
-        </Button>
-        {shouldDownload && CSVdata && (
-          <CSVDownload
-            data={CSVdata}
-            filename={`klusteri-events-${date}.csv`}
-            target="_blank"
+          <div style={{ float: "right" }}>
+            <Button type="button" variant="contained" onClick={handleCSV}>
+              Lataa CSV-tiedosto tapahtumista
+            </Button>
+            {shouldDownload && CSVdata && (
+              <CSVDownload
+                data={CSVdata}
+                filename={`klusteri-events-${date}.csv`}
+                target="_blank"
+              />
+            )}
+          </div>
+        </Grid>
+        <Grid item xs={columnWidth}>
+          <h2>YKV-kirjausten määrä järjestöittäin</h2>
+          <PieChart
+            series={[
+              {
+                data: orgStatsData,
+              },
+            ]}
+            width={winWidth / widthDivider}
+            height={winHeight / 2.5}
           />
-        )}
-        </div>
         </Grid>
         <Grid item xs={columnWidth}>
-        <h2>YKV-kirjausten määrä järjestöittäin</h2>
-        <PieChart
-        series={[
-        {
-          data: orgStatsData,
-        },
-        ]}
-        width={winWidth / widthDivider}
-        height={winHeight / 2.5}
-        />
+          <h2>YKV-kirjausten määrä käyttäjittäin</h2>
+          <BarChart
+            width={winWidth / widthDivider}
+            height={winHeight / 2.5}
+            series={allUserStatsData}
+            yAxis={[{ data: [""], scaleType: "band", barGapRatio: 0.1 }]}
+            layout="horizontal"
+          />
         </Grid>
         <Grid item xs={columnWidth}>
-        <h2>YKV-kirjausten määrä käyttäjittäin</h2>
-        <BarChart
-        width={winWidth / widthDivider}
-        height={winHeight / 2.5}
-        series={allUserStatsData}
-        yAxis={[{ data: [""], scaleType: "band", barGapRatio: 0.1 }]}
-        layout="horizontal"
-        />
+          <h2>YKV-kirjausten määrä tunneittain</h2>
+          <BarChart
+            series={logTimesData || []}
+            width={winWidth / widthDivider}
+            height={winHeight / 2.5}
+            borderRadius={5}
+          />
         </Grid>
         <Grid item xs={columnWidth}>
-        <h2>YKV-kirjausten määrä tunneittain</h2>
-        <BarChart
-        series={logTimesData || []}
-        width={winWidth / widthDivider}
-        height={winHeight / 2.5}
-        borderRadius={5}
-        />
-        </Grid>
-        <Grid item xs={columnWidth}>
-        <h2>YKV-kirjausten määrä viikoinpäivittäin</h2>
-        <BarChart
-        series={logsPerWeekDayData || []}
-        xAxis={[
-          {
-            scaleType: 'band',
-            data: [
-              "ma",
-              "ti",
-              "ke",
-              "to",
-              "pe",
-              "las",
-              "su"
-            ],
-          }
-        ]}
-        width={winWidth / widthDivider}
-        height={winHeight / 2.5}
-        borderRadius={5}
-        />
+          <h2>YKV-kirjausten määrä viikoinpäivittäin</h2>
+          <BarChart
+            series={logsPerWeekDayData || []}
+            xAxis={[
+              {
+                scaleType: "band",
+                data: ["ma", "ti", "ke", "to", "pe", "la", "su"],
+              },
+            ]}
+            width={winWidth / widthDivider}
+            height={winHeight / 2.5}
+            borderRadius={5}
+          />
         </Grid>
       </Grid>
-
     </div>
   );
 };
