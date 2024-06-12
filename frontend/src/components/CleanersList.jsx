76ad92410523128bf992c09298c8a89046dd63ca
Olivia Brotherus <olivia@brotherus.fi>
2024-06-12 18:51:34 +0300

    korjaa muotoilua

--- frontend/src/components/CleanersList.jsx
+++ frontend/src/components/CleanersList.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { styled } from "@mui/material/styles";
+import { lighten, styled } from '@mui/material/styles';
 import moment from "moment";
 
-const CleanersList = ({ allCleaners}) => {
+
+const CleanersList = ({ allCleaners }) => {
+  const getBackgroundColor = (color) =>
+    lighten(color, 0.4);
+  
+  const getHoverBackgroundColor = (color) =>
+    lighten(color, 0.2);
+
+  const getSelectedBackgroundColor = (color) =>
+    lighten(color, 0.1);
+  
+  const getSelectedHoverBackgroundColor = (color) =>
+    lighten(color, 0.1);
+
   const StyledDataGrid = styled(DataGrid)(({ theme }) => ({
     "& .past-week": {
-      backgroundColor: theme.palette.grey[300],
-      "&:hover": {
-        backgroundColor: theme.palette.grey[400],
+      backgroundColor: getBackgroundColor(theme.palette.grey[300]),
+      transition: 'background-color 0.1s ease',
+      '&:hover': {
+        backgroundColor: getHoverBackgroundColor(theme.palette.grey[300]),
+      },
+      '&.Mui-selected': {
+        backgroundColor: getSelectedBackgroundColor(theme.palette.grey[300]),
+        '&:hover': {
+          backgroundColor: theme.palette.grey[300],
+        },
       },
     },
   }));
@@ -20,27 +40,23 @@ const CleanersList = ({ allCleaners}) => {
     { field: "small", headerName: "Pieni järjestö", width: 120 },
   ];
 
-  const today = moment();
-
   const getRowClassName = (params) => {
-    const rowDate = moment(params.row.date);
-    return rowDate.isBefore(today, "day") ? "past-week" : "";
+    const rowWeek = moment(params.row.date).week();
+    const currentWeek = moment().week();
+    return rowWeek < currentWeek ? "past-week" : "";
   };
 
-  return (
-    <StyledDataGrid
-      initialState={{
-        sorting: {
-          sortModel: [{ field: "week", sort: "asc" }],
-        },
-      }}
-      rows={allCleaners}
-      columns={columns}
-      pageSize={5}
-      rowsPerPageOptions={[5, 10, 20]}
-      getRowClassName={getRowClassName}
-    />
-  );
+    return (
+        <StyledDataGrid
+        initialState={{sorting:{sortModel:[{field:'week', sort:'asc'}],
+          },}}
+        rows={allCleaners}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5, 10, 20]}
+        getRowClassName={getRowClassName}
+        />
+    );
 };
 
 export default CleanersList;
