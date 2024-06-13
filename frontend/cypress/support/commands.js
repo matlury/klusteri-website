e311e417cfc0ac93d6185f1a7ce5a6d5e6971e93
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-06-13 11:36:50 +0300

    Add cypress-downloadfile plugin

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -1,3 +1,6 @@
+
+require('cypress-downloadfile/lib/downloadFileCommand')
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
