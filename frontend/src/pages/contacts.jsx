ea70a0b7582115576338dbe3093cc393b21b4ff6
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-06-14 15:01:24 +0300

    Stop automatic reload with Link & update jest

--- frontend/src/pages/contacts.jsx
+++ frontend/src/pages/contacts.jsx
@@ -14,16 +14,16 @@ const Contacts = () => {
       <p>
         {t("contacts_1")}
       </p>
-      <a href="/christina_regina">
-        <h1 onClick={OpenChristinaRegina}>Christina Regina</h1>
-      </a>
+      <Link to="/christina_regina">
+        <h1>Christina Regina</h1>
+      </Link>
       <p>
         {t("contacts_2")}
       </p>
       <h1>{t("contacts_3")}</h1>
       <p>{t("contacts_4")}</p>
       <h1>{t("contacts_5")}</h1>
-      <p>{t("contacts_6")}</p>
+      <p>{t("contacts_6")}</p> 
       <p>{t("contacts_7")}</p>
       <h1>{t("contacts_8")}</h1>
     </div>
--- frontend/src/tests/contacts.test.js
+++ frontend/src/tests/contacts.test.js
@@ -1,30 +1,40 @@
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import Contacts from "../../src/pages/contacts";
 import i18n from "../i18n.js";
 
-localStorage.setItem("lang", "fi")
+localStorage.setItem("lang", "fi");
 
-jest.mock("react", () => ({
-  ...jest.requireActual("react"),
-  createElement: jest.fn(),
-}));
+const ChristinaReginaComponent = () => (
+  <div>
+    <h1>Christina Regina Page</h1>
+  </div>
+);
 
-test("redirects to Christinaregina page when the link button is clicked", () => {
-  const openSpy = jest.spyOn(window, "open").mockImplementation(() => {});
+test("redirects to ChristinaRegina page when the link button is clicked", () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={["/contacts"]}>
+      <Routes>
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/christina_regina" element={<ChristinaReginaComponent />} />
+      </Routes>
+    </MemoryRouter>
+  );
 
-  const { getByText } = render(<Contacts />);
   const linkButton = getByText("Christina Regina");
   fireEvent.click(linkButton);
 
-  expect(openSpy).toHaveBeenCalledWith("/christina_regina", "_self");
-
-  // Restore the original window.open after the test
-  openSpy.mockRestore();
+  // Ensure the new page has rendered by checking the content of ChristinaRegina
+  expect(getByText("Christina Regina Page")).toBeInTheDocument();
 });
 
 test("renders Contacts Page component", () => {
-  const { getByText } = render(<Contacts />);
+  const { getByText } = render(
+    <MemoryRouter>
+      <Contacts />
+    </MemoryRouter>
+  );
 
   expect(getByText("Christina Regina")).toBeInTheDocument();
   expect(getByText("Domus Gaudium")).toBeInTheDocument();
