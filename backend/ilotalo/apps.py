3ea8bdd201685bcc606e15bcf5ca5992af609f42
Olivia Brotherus <olivia@brotherus.fi>
2024-06-17 13:27:02 +0300

    luo leppispj jos tietokanta tyhjä

--- backend/ilotalo/apps.py
+++ backend/ilotalo/apps.py
@@ -1,8 +1,20 @@
 from django.apps import AppConfig
 from django.db import connection
+from django.db.backends.signals import connection_created
 from django.db.utils import OperationalError
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
 
-"""Configure apps created in the project root"""
+def create_default_user(sender, **kwargs):
+    User = get_user_model()
+    try:
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_user('leppispj', 'salasana123', 'pj@leppis.fi', "", 1)
+            print("Default admin user created")
+        else:
+            print("Default admin user already exists")
+    except OperationalError:
+        pass
 
 class IlotaloConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
@@ -10,17 +22,17 @@ class IlotaloConfig(AppConfig):
 
     def ready(self):
         self.start_scheduler()
+        connection_created.connect(create_default_user)
 
     def start_scheduler(self):
         try:
             if self._check_scheduler_tables():
                 from scheduler import scheduler
                 scheduler.start()
+
         except OperationalError:
-            # If the database is not set up yet, skip starting the scheduler
             pass
 
     def _check_scheduler_tables(self):
-        """Check if APScheduler tables exist in the database."""
         table_names = connection.introspection.table_names()
         return "django_apscheduler_djangojob" in table_names and "django_apscheduler_djangojobexecution" in table_names
