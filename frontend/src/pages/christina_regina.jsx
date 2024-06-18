c13c1462d296594d73e5ee1a2a0cecd021763679
Janne <48507101+janneko01@users.noreply.github.com>
2024-06-18 13:01:34 +0300

    Make pages more mobile frienfly

--- frontend/src/pages/christina_regina.jsx
+++ frontend/src/pages/christina_regina.jsx
@@ -1,23 +1,50 @@
 import React from "react";
 import christinaregina from "../ChristinaRegina.png";
 import { useTranslation } from "react-i18next";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
 
 const ChristinaRegina = () => {
   const { t } = useTranslation();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
-    <div className="textbox">
-      <h1>Christina Regina</h1>
-      <p>
+    <Box
+      sx={{
+        padding: 2,
+        maxWidth: "600px",
+        margin: "0 auto",
+        textAlign: "center",
+      }}
+    >
+      <Typography
+        variant={isMobile ? "h4" : "h2"}
+        component="h1"
+        gutterBottom
+      >
+        Christina Regina
+      </Typography>
+      <Typography variant="body1" paragraph>
         {t("christina_regina_1")}
-      </p>
-      <p>
+      </Typography>
+      <Typography variant="body1" paragraph>
         {t("front_1")} {t("front_2")} {t("christina_regina_2")}
-      </p>
-      <p>
+      </Typography>
+      <Typography variant="body1" paragraph>
         {t("christina_regina_3")}
-      </p>
-      <img src={christinaregina} id="map" width="400" height="300" alt="Logo" />
-    </div>
+      </Typography>
+      <Box
+        component="img"
+        src={christinaregina}
+        alt="Christina Regina"
+        sx={{
+          display: "block",
+          maxWidth: "100%",
+          height: "auto",
+          margin: "0 auto",
+        }}
+      />
+    </Box>
   );
 };
 
