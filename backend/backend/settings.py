6e0a910637e03326cef230e5dcdad3bdfe2a22dc
oskarruo <oskar.ruokolainen@gmail.com>
2024-06-19 15:27:09 +0300

    lisätty djangon secret key env muuttujiin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5yvx^z)huz-zg-px7102vn#yi5i#s*93^p8a2$*&_l@@#@fu!w"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-5yvx^z)huz-zg-px7102vn#yi5i#s*93^p8a2$*&_l@@#@fu!w")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
