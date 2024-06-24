1924f589b2702a9c3520c2b7acc4e8e385558741
oskarruo <oskar.ruokolainen@gmail.com>
2024-06-24 13:58:53 +0300

    lisätty avoimet tapahtumat etusivulle

---
+++ frontend/src/components/FrontpageEvents.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import CardActions from '@mui/material/CardActions';
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogActions from "@mui/material/DialogActions";
+import Typography from '@mui/material/Typography';
+import { useTranslation } from "react-i18next";
+
+const FrontpageEvents = ({ events }) => {
+  const { t } = useTranslation();
+  const [openDialogId, setOpenDialogId] = useState(null);
+
+  const handleClickOpen = (eventId) => {
+    setOpenDialogId(eventId);
+  };
+
+  const handleClose = () => {
+    setOpenDialogId(null);
+  };
+
+  return (
+    <>
+      {events.map((event) => (
+        <div key={event.id}>
+          <Card sx={{ minWidth: 275, margin: 1, boxShadow: 4 }} variant="outlined">
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {event.title} - {event.organizer.name}
+              </Typography>
+              <Typography color="text.secondary">
+                {event.start.toLocaleDateString('no-NO', {
+                  year: 'numeric',
+                  month: 'numeric',
+                  day: 'numeric',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                })}{" "}
+                @ {event.room}
+              </Typography>
+            </CardContent>
+            <CardActions sx={{ justifyContent: 'center' }}>
+              <Button size="small" onClick={() => handleClickOpen(event.id)}>{t("moredetails")}</Button>
+            </CardActions>
+          </Card>
+          <Dialog key={event.id} open={openDialogId === event.id} onClose={handleClose}>
+            <DialogTitle>{event.title} - {event.organizer.name}</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                {event.description}
+                <br />
+                <br />
+                {event.start.toLocaleDateString('no-NO', {
+                  year: 'numeric',
+                  month: 'numeric',
+                  day: 'numeric',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                })}{" "}
+                - {event.end.toLocaleDateString('no-NO', {
+                  year: 'numeric',
+                  month: 'numeric',
+                  day: 'numeric',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                })}{" "}
+                @ {event.room}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                {t("close")}
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default FrontpageEvents;
--- frontend/src/pages/frontpage.jsx
+++ frontend/src/pages/frontpage.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import FrontpageEvents from "../components/FrontpageEvents";
+import axios from "axios";
+
+const API_URL = process.env.API_URL;
 
 const FrontPage = () => {
   const { t } = useTranslation();
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const [events, setEvents] = useState(null)
+
+  useEffect(() => {
+    axios
+    .get(`${API_URL}/api/listobjects/events/`)
+    .then((response) => {
+      const events = response.data.filter((event) => new Date() < new Date(event.start) &&
+      event.open == true)
+      .map((event) => ({
+        ...event,
+        start: new Date(event.start),
+        end: new Date(event.end),
+      }))
+      .sort((a, b) => new Date(a.start) - new Date(b.start))
+      .slice(0, 10);
+      setEvents(events);
+    })
+    .catch((error) => {
+      console.error(t("errorfetchevents"), error);
+    });
+  }, [])
 
   return (
+    <div>
     <Box
       sx={{
         padding: 2,
@@ -33,6 +59,26 @@ const FrontPage = () => {
         {t("front_2")}
       </Typography>
     </Box>
+    <Box
+    sx={{
+      padding: 2,
+      maxWidth: "600px",
+      margin: "0 auto",
+      textAlign: "center",
+    }}
+    >
+      <Typography
+        variant={isMobile ? "h6" : "h4"}
+        component="h1"
+        gutterBottom
+      >
+        {t("openevents")}
+      </Typography>
+      {events === null ? (
+        <Typography>{t("loadingevents")}</Typography>
+      ) : <FrontpageEvents events={events}/>}
+    </Box>
+    </div>
   );
 };
 
