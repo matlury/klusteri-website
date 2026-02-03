2d957e8fb59303eae1329f18bac70008f98955da
iPegii <51372604+iPegii@users.noreply.github.com>
2026-02-03 14:29:37 +0200

    revert changes to API_URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import envCompatible from 'vite-plugin-env-compatible';
-console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
+console.log("VITE_API_URL:", process.env.VITE_API_URL);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), envCompatible()],
@@ -10,10 +10,10 @@ export default defineConfig({
   },
   define: {
     "process.env.VITE_API_URL": JSON.stringify(
-      import.meta.env.VITE_API_URL || "http://localhost:8000"
+      process.env.VITE_API_URL || "http://localhost:8000"
     ),
     "process.env.VITE_SITE_KEY": JSON.stringify(
-      import.meta.env.VITE_SITE_KEY || "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" // reCAPTCHA site key, uses the testing key from Google if not set
+      process.env.VITE_SITE_KEY || "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" // reCAPTCHA site key, uses the testing key from Google if not set
     ),
   },
 });
