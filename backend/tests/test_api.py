2ee2465c6499e0c980eb432adb5996576369bb12
iPegii <51372604+iPegii@users.noreply.github.com>
2026-02-08 01:21:06 +0200

    fix the new api test for password changing

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -2258,37 +2258,32 @@ class TestDjangoAPI(TestCase):
         )
 
         self.assertNotEqual(old_token_resp.status_code, status.HTTP_200_OK)
-        """An authorized user can update their email address"""
 
-        # update the email address
-        user_id = User.objects.all()[0].id
+        # Test updating own email
         response = self.client.put(
-            f"http://localhost:8000/api/users/update/{user_id}/",
+            f"http://localhost:8000/api/users/update/{self.tavallinen_id}/",
             headers={"Authorization": f"Bearer {self.access_token}"},
-            data={"password": "newpassword123"},
+            data={"email": "new.email@example.com"},
             format="json",
         )
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        # leppispj user can update any password
-        user_id = User.objects.all()[1].id
+        # leppispj user can update any user's password
+        # Update klusse's password as LeppisPJ
         response = self.client.put(
-            f"http://localhost:8000/api/users/update/{user_id}/",
+            f"http://localhost:8000/api/users/update/{self.tavallinen_id}/",
             headers={"Authorization": f"Bearer {self.leppis_access_token}"},
-            data={"password": "newpassword123"},
+            data={"password": "leppis_changed_it"},
             format="json",
         )
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        # other users can't update other users' passwords
-        user_id = User.objects.all()[0].id
+        # other users can't update other users' data
+        # klusse (role 5) tries to update LeppisPJ's data
         response = self.client.put(
-            f"http://localhost:8000/api/users/update/{user_id}/",
+            f"http://localhost:8000/api/users/update/{self.leppis_id}/",
             headers={"Authorization": f"Bearer {self.access_token}"},
-            data={"password": "newpassword123"},
+            data={"email": "hacked@example.com"},
             format="json",
         )
-
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
