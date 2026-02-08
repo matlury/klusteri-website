f61b731ac83bdb64a9d0cbabc7e5f00d6b9c6dcc
iPegii <51372604+iPegii@users.noreply.github.com>
2026-02-08 18:53:11 +0200

    add django_scheduler back in settings.py also

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
     "ilotalo",
-    # 'django_apscheduler'  # DISABLED: Causes slow startup in production
+    "django_apscheduler",
 ]
 
 MIDDLEWARE = [
